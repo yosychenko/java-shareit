27de029ecf9920e03a4d335763f815159ea99f9e
Osychenko Yuriy Vsevolodovich <yosychenko@yandex.ru>
2023-04-30 18:05:55 +0300

    Add volumes for log access, create run scripts

--- gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
+++ gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
@@ -1,12 +1,13 @@
 package ru.practicum.shareit;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class ShareItGateway {
     public static void main(String[] args) {
         SpringApplication.run(ShareItGateway.class, args);
     }
-
 }
--- server/src/main/java/ru/practicum/shareit/ShareItServer.java
+++ server/src/main/java/ru/practicum/shareit/ShareItServer.java
@@ -1,12 +1,21 @@
 package ru.practicum.shareit;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
+@Slf4j
 public class ShareItServer {
+    @PostConstruct
+    public void startupApplication() {
+        log.info("Starting application with JAVA_OPTS: " + System.getenv("JAVA_OPTS"));
+    }
 
     public static void main(String[] args) {
+
         SpringApplication.run(ShareItServer.class, args);
     }
 
